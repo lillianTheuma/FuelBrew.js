19a20ef5cac036421f70d20b8e2499506b23c1be
Lillian Theuma <definatelyruby@gmail.com>
2020-02-16 13:08:42 -0800

    removed bootstrap, moved around file structure as a test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Header from "./Header";
-import { Switch, Route } from 'react-router-dom';
 import Main from './Main';
-import Homepage from './Homepage';
-import NewBrew from './NewBrew';
-import AboutUs from './AboutUs';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Container from 'react-bootstrap/Container'
 
 
 function App(){
 
   return (
-    <Container>
+    <div>
       <Header/>
       <Main/>
-    </Container>
+    </div>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,41 @@
 import React from "react";
 import Homepage from './Homepage';
 import NewBrew from './NewBrew';
-import AboutUs from './AboutUs';
+import AboutUs from './aboutUs/index';
 import { Switch, Route } from 'react-router-dom';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 function Content(){
-
+  const gridContainer = {
+    "display": "grid",
+    "grid-template-columns": "auto auto auto auto",
+    "grid-template-rows": "100px 300px",
+    "grid-gap": "10px",
+    "padding": "10px",
+  }
+  const gridContainerDiv = {
+    "background-color": "rgba(255, 255, 255, 0.8)",
+    "text-align": "center",
+    "padding": "20px 0",
+    "font-size": "30px",
+    "margin":"auto"
+  }
+  const border = {
+    "width":"30%"
+  }
   return (
     <div>
-    <p> Main Body! </p>
-    <Container>
-    <Row>
-      <Col xs>First, but unordered</Col>
-      <Col xs={{ order: 12 }}>Second, but last</Col>
-      <Col xs={{ order: 1 }}>Third, but second</Col>
-    </Row>
-  </Container>
-    <Switch>
-      <Route exact path='/' component={Homepage}/>
-      <Route exact path='/newBrew' component={NewBrew}/>
-      <Route exact path='/aboutUs' component={AboutUs}/>
-    </Switch>
-    </Row>
+      <p> Main Body! </p>
+      <div style={gridContainer}>
+        <div style={border}></div>
+        <div style={gridContainerDiv}>
+          <Switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/newBrew' component={NewBrew}/>
+          <Route exact path='/aboutUs' component={AboutUs}/>
+          </Switch>
+        </div>
+        <div style={border}></div>
+      </div>
     </div>
   );
 }
